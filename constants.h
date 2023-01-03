48f8f77104037060fd2c6702ff6bd99ef77c440d
turkay.biliyor <turkay.biliyor@iys.org.tr>
2023-01-03 11:01:51 +0300

    Update constants.h

--- constants.h
+++ constants.h
@@ -3,23 +3,6 @@
 #include <QString>
 
 static QString baseApi = "https://speech.googleapis.com/v1/speech:recognize";
-static QString apiKey = "AIzaSyCY8Xg5wfn6Ld67287SGDBQPZvGCEN6Fsg";
+static QString apiKey = "AIzaSyCY8Xg5wfn6silLd67287SGDBQPZvGCEN6Fsg";
 
 #endif // CONSTANTS_H
-
-
-//QAudioFormat desiredFormat;
-//desiredFormat.setChannelCount(2);
-//desiredFormat.setCodec("audio/x-raw");
-//desiredFormat.setSampleType(QAudioFormat::UnSignedInt);
-//desiredFormat.setSampleRate(48000);
-//desiredFormat.setSampleSize(16);
-
-//QAudioDecoder *decoder = new QAudioDecoder(this);
-//decoder->setAudioFormat(desiredFormat);
-//decoder->setSourceFilename("level1.mp3");
-
-//connect(decoder, SIGNAL(bufferReady()), this, SLOT(readBuffer()));
-//decoder->start();
-
-//// Now wait for bufferReady() signal and call decoder->read()
